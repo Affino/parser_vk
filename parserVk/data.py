bec9be0266f701b13dda12ec2cc7c5244eae2b27
Affino <affino45@gmail.com>
2023-02-10 12:59:51 +0600

    added documentation for the class

--- parserVk/data.py
+++ parserVk/data.py
@@ -1,20 +1,30 @@
 from .captcha import auth_vk
 
+# Список полей для того чтобы видеть нужную информацию
 USER_FIELDS = "photo_max," "domain," "sex," "bdate," \
               "followers_count," "online," "last_seen,"  \
-              "contacts," "status,"
+              "contacts," "status," "city," "country,"  \
+              "site," "verified," "about,"
 
-GROUP_FIELDS = 'members_count,' 'status,' 'description'
+GROUP_FIELDS = 'verified,' 'members_count,' 'status,' 'description,' 'city,' 'country,' 'site,'
 
 vk = auth_vk()
 
 
 class Data:
+    """
+        Собираем данные пользователя и группы вконтакте.
+        Данные собирается с помощью библиотекой vk_api.
+        Познакомится с библиотекой можете здесь --> https://dev.vk.com/reference
+    """
     def __init__(self):
         self._uncollected_data = []
 
 
 class UserData(Data):
+    """ Получаем данные пользователя методом users.get
+        Подробнее о методе можете узнать по ссылке --> https://dev.vk.com/method/users.get
+    """
     def get(self, user_ids):
         data = vk.users.get(user_ids=user_ids, fields=USER_FIELDS)
         for i in data:
@@ -23,6 +33,9 @@ class UserData(Data):
 
 
 class GroupData(Data):
+    """ Получаем данные группы методом getById.get
+        Подробнее о методе можете узнать по ссылке --> https://dev.vk.com/method/groups.getById
+    """
     def get(self, group_ids):
         data = vk.groups.getById(group_ids=group_ids, fields=GROUP_FIELDS)
         for i in data:
