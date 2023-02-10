a373cfeaab4701652faa31a4d4f781c1285703a4
Affino <affino45@gmail.com>
2023-02-10 13:27:48 +0600

    refactoring

--- main.py
+++ main.py
@@ -2,51 +2,50 @@ from itertools import zip_longest
 import time
 
 from parserVk.data import *
-from parserVk.extract import extract_user_data, extract_group_data
+from parserVk.extract import filter_user_data, filter_group_data
 from parserVk.save import save
 from mysql.execute import Query
 
 
 query = Query('db')
 data = Data()
-COLUMN_USER = 'first_name=Null, last_name=Null, photo=Null, user_url=Null,' \
-              'gender=Null, bdate=Null, followers_count=Null, online_=Null,' \
-              'last_seen=Null, mobile_phone=Null, user_status=Null'
-COLUMN_GROUP = 'title=Null, group_url=Null, members_count=Null, ' \
-               'group_status=Null, description=Null, type=Null, is_closed=Null'
 
 
 # -----USER DATA-----
 def parse_users(ids):
+    """ Парсить пользователя вконтакте с помощью vk id"""
     user_data = UserData()
-    uncollected_data = user_data.get(ids)
-    for i, user_id in zip_longest(uncollected_data, ids):
-        collected_data = extract_user_data(i)
-        save('vk_info', collected_data, query, 'user_id', user_id)
+    unfiltered_data = user_data.get(ids)
+    for unf_data, vk_id in zip_longest(unfiltered_data, ids):
+        filtered_data = filter_user_data(unf_data)
+        save('vk_info', filtered_data, query, 'vk_id', vk_id)
 
 
 # -----GROUP DATA-----
 def parse_groups(ids):
+    """ Парсить группы вконтакте с помощью vk id"""
     group_data = GroupData()
-    uncollected_data = group_data.get(ids)
-    for i, group_id in zip_longest(uncollected_data, ids):
-        collected_data = extract_group_data(i)
-        save('vk_info', collected_data, query, 'group_id', group_id)
-        query.set_null('vk_info', COLUMN_GROUP, 'group_id')
+    unfiltered_data = group_data.get(ids)
+    for unf_data, vk_id in zip_longest(unfiltered_data, ids):
+        filtered_data = filter_group_data(unf_data)
+        save('vk_info', filtered_data, query, 'vk_id', f'-{vk_id}')
 
 
 def main():
-    # -----ID-----
-    ids = query.select_value('vk_info', 'user_id')
-    user_ids = [i for i in ids if i is not None]
-    ids = query.select_value('vk_info', 'group_id')
-    group_ids = [i for i in ids if i is not None]
+    ids = query.select_value('vk_info', 'vk_id')  # получаем идентификаторы пользователя и группы
+    # --- VK ID USERS
+    user_ids = [i for i in ids if i is not None and '-' not in str(i)]
+    # --- VK ID GROUPS ---
+    group_ids_with_minus = [i for i in ids if i is not None and '-' in str(i)] # идентификаторы группы с знаком минусум
+    group_ids = []  # идентификаторы группы без знака минуса
+    for i in group_ids_with_minus:
+        group_id = str(i).replace('-', '')
+        group_ids.append(int(group_id))
+
     parse_users(user_ids)
     parse_groups(group_ids)
 
 
 if __name__ == '__main__':
-    # --- Улучшение: Программа не может очистить лишние данные
-        # Добавить сортировку по возрастание id в метод select_value() √
     while True:
         main()
